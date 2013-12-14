550692849b7a699a4d11cc6a4b13b6c74a5eaf93
Mark <wei.xiaoma@gmail.com>
2013-12-14 00:26:16 -0800

    adds individual product views and kickass barcode generation code http://localhost:9000/products/5010255079763

--- app/models/Product.scala
+++ app/models/Product.scala
@@ -18,4 +18,5 @@ object Product {
   )
 
   def findAll = products.toList.sortBy(_.ean)
+  def findByEan(ean: Long) = products.find(_.ean == ean)
 }
--- conf/messages.es
+++ conf/messages.es
@@ -1,1 +1,5 @@
 application.name = Catálogo de productos
+name = Nombre
+description = Descripción
+
+products.details = Producto: {0}
